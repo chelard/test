dc53cb08e76e457a2443e086e34d7d09bfde36c6
helard flores <helard@gmail.com>
2022-12-20 00:22:03 -0500

    datos iniciales

---
+++ app/Models/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['names', 'email', 'password'];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *s
+     * @var array<int, string>
+     */
+    protected $hidden = ['password', 'remember_token'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    //get user data in array
+    public function get_data()
+    {
+        return [
+            'id' => $this->id,
+            'names' => $this->names,
+            'email' => $this->email,
+            'email_verified_at' => $this->email_verified_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
